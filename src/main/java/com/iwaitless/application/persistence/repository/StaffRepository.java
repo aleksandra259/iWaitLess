9ea3dda60bfdc4458167e5115b1d2e8c3643af82
aleksandra259 <alex_6666@abv.bg>
2024-01-30 18:51:46 +0200

    Fix

--- src/main/java/com/iwaitless/application/persistence/repository/StaffRepository.java
+++ src/main/java/com/iwaitless/application/persistence/repository/StaffRepository.java
@@ -1,7 +1,7 @@
 package com.iwaitless.application.persistence.repository;
 
 
-import com.iwaitless.application.persistence.entity.Ingredients;
+import com.iwaitless.application.persistence.entity.Staff;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface StaffRepository extends JpaRepository<Staff, Long> {
